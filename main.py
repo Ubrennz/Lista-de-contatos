9aaccfa20bef1b49cdd69038cc2adecd84aa5758
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2026-05-28 16:13:41 -0300

    arquivo main funcional

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
-from repository.csv_repository import add_dados_arquivo, atualizar_dados, apagar_dados
+from repository.csv_repository import adicionar_dados, leitor_dados, atualizar_dados, apagar_dado
+from time import sleep
 
-caminho1 = "teste.csv"
+def criar_contato():
+    nome_contato = input("Digite o nome do contato: ")
+    numero_contato = input("Digite o número do contato: ")
+    adicionar_dados(caminho, "a", [[nome_contato, numero_contato]])
 
-add_dados_arquivo(caminho1, "w", [["teste", "1", "2"]])
-atualizar_dados(caminho1, "teste", "novo_teste")
-apagar_dados(caminho1, "novo_teste")
-apagar_dados(caminho1, "2")
+def mostrar_contatos():
+    for linha in leitor_dados(caminho):
+        print(f"{linha[0]} - {linha[1]}")
+
+def atualizar_contato():
+    dado_antigo = input("Digite um valor que você deseja atualizar: ")
+    dado_novo = input("Digite o novo valor: ")
+    atualizar_dados(caminho, dado_antigo, dado_novo)
+
+def apagar_contato():
+    dado_para_apagar = input("Digite o valor que você deseja apagar: ")
+    apagar_dado(caminho, dado_para_apagar)
+
+def interacao_saindo():
+    print("Saindo", end="")
+
+    for c in range(0, 3):
+        sleep(0.3)
+        print(".", end="")
+    print()
+
+caminho = "arquivo.csv"
+
+opcoes = {
+        "1": "Criar registro",
+        "2": "Listar todos",
+        "3": "Atualizar registro",
+        "4": "Deletar registro",
+        "0": "Sair",
+    }
+
+while True:
+    for v, k in opcoes.items():
+        print(f"[{v}] {k}")
+    opcao = int(input("Digite a opcao: "))
+
+    if opcao == 1:
+        criar_contato()
+    elif opcao == 2:
+        mostrar_contatos()
+    elif opcao == 3:
+        atualizar_contato()
+    elif opcao == 4:
+        apagar_contato()
+    elif opcao == 0:
+        interacao_saindo()
+        break
+    else:
+        print("Número inválido, digite outro número")
