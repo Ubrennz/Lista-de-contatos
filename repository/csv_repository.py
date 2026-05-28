4a3cec2bec884255918e4899bb443ee236153121
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2026-05-28 16:14:12 -0300

    mudanças nos nomes das funções

--- repository/csv_repository.py
+++ repository/csv_repository.py
@@ -1,6 +1,6 @@
 import csv
 
-def add_dados_arquivo(caminho: str, modo: str, dados: list):
+def adicionar_dados(caminho: str, modo: str, dados: list):
     with open(caminho, modo, newline="",encoding="utf-8") as arquivo:
         escritor = csv.writer(arquivo)
         escritor.writerows(dados)
@@ -18,15 +18,15 @@ def atualizar_dados(caminho: str, dado_antigo: str, novo_dado: str):
                 dados_arquivo[index1][index2] = novo_dado
                 break
 
-    add_dados_arquivo(caminho, "w", dados_arquivo)
+    adicionar_dados(caminho, "w", dados_arquivo)
 
-def apagar_dados(caminho: str, dado_para_apagar: str):
+def apagar_dado(caminho: str, dado_para_apagar: str):
     dados_arquivo = leitor_dados(caminho)
 
     for index1, linha in enumerate(dados_arquivo):
         for index2 in range(0, len(linha)):
             if dados_arquivo[index1][index2] == dado_para_apagar:
-                del dados_arquivo[index1][index2]
+                dados_arquivo[index1][index2] = ""
                 break
 
-    add_dados_arquivo(caminho, "w", dados_arquivo)
+    adicionar_dados(caminho, "w", dados_arquivo)
